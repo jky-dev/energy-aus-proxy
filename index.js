617839f6491186a4bb830ab80878d6d0dccbee57
Jacky Liang <liangj97@gmail.com>
2019-10-04 18:30:28 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+  request(
+    { url: 'http://eacodingtest.digital.energyaustralia.com.au/api/v1/festivals' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
